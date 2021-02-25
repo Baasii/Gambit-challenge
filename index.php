4a1b05587f39ee35d603b3808e2f11cf959a0600
Lauri Mäntylä <lauri.ma@hotmail.com>
2021-02-25 14:35:10 +0200

    Moved CSS into seperate folder

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once "header.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Challenge</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
 
 <body>
